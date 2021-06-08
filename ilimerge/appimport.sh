e4d48f7dbbd865889a23a79777cff3a17acedef0
uncoyote <jeker.git@outlook.de>
2021-06-08 05:44:49 +0200

    added geodata db content to doc

--- ilimerge/appimport.sh
+++ ilimerge/appimport.sh
@@ -4,4 +4,4 @@ java -jar ~/tools/ili2pg-4.3.1/ili2pg-4.3.1.jar \
 --import \
 --dbhost localhost --dbport 5432 --dbdatabase postgres --dbschema abbau_ilimerge --dbusr postgres --dbpwd postgres \
 --dataset app --replace \
-$(pwd)/appdata_app3geo2.xtf
+$(pwd)/appdata_app1geo2.xtf
--- ilimerge/geoexport.sh
+++ ilimerge/geoexport.sh
@@ -4,4 +4,4 @@ java -jar ~/tools/ili2pg-4.3.1/ili2pg-4.3.1.jar \
 --export \
 --dbhost localhost --dbport 5432 --dbdatabase postgres --dbschema abbau_ilimerge --dbusr postgres --dbpwd postgres \
 --dataset geo --disableValidation \
-$(pwd)/gitignored/geodata_exported.xtf
+$(pwd)/geodata_exported.xtf
